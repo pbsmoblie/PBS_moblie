51d36aea993c0a70df6dd03c32e8ceb398562b28
seung-aa <baeka0202@gmail.com>
2020-06-14 16:00:46 +0900

    뚜버기

---
+++ app/src/main/java/com/example/pbs_moblie/NickActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pbs_moblie
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_nick.*
+
+class NickActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nick)
+
+        var intent = Intent(this,MainActivity::class.java)
+        nic_btn.setOnClickListener {
+           startActivity(intent)
+        }
+    }
+}
